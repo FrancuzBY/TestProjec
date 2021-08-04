562e5e071d6f4d6001b10a03eb2365edeecfb282
Толстик <francuzby@gmail.com>
2021-08-04 11:40:29 +0300

    Hello

--- src/Solution.java
+++ src/Solution.java
@@ -4,8 +4,8 @@ public class Solution{
 
     public static void main(String[] args) {
 
-        System.out.println("I am looking for a job as a Java developer. Write, we will discuss.\n" +
-                            "");
+        System.out.println("I am looking for a job as a Java developer. Write, we will discuss");
+        System.out.println("Hello");
 
     }
 
